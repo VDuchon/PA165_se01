b426d0e62547ef23777fb40b0a9dcb4b383fbf9d
Vojtech Duchon <xduchon1@nymfe36.fi.muni.cz>
2017-09-20 09:11:11 +0200

    first commit

---
+++ src/main/java/package_value/App.java
@@ -0,0 +1,13 @@
+package package_value;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
